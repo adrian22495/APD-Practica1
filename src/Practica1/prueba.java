366d1818f7481442f376e9602bb2344304fdb8a1
adrian alejandre escriche <adrian alejandre escriche>
2016-11-21 19:23:05 +0100

    probando que vaya

---
+++ src/Practica1/prueba.java
@@ -0,0 +1,11 @@
+package Practica1;
+
+/**
+ * Created by adrian on 21/11/2016.
+ */
+public class prueba{
+    public static void main(String[] args){
+
+        System.out.printf("funciono");
+    }
+}
